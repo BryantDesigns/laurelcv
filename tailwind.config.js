a01d46cd90d6130f23afef3d38b44bc138783c54
Tyler Bryant <tbryantdesignsllc@gmail.com>
2024-11-30 09:53:53 -0700

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html", // Include HTML files in the public folder
+    "./src/**/*.{html,js}" // Include other files if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
